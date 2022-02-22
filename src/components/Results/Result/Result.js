2e5668a57a493b40180c399365319d972a313629
allendavid85 <54120819+allendavid85@users.noreply.github.com>
2022-02-22 13:19:18 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -6,7 +6,7 @@ export default function Result(props) {
     return(
     <div className="card result">
         <a href={`/details/${props.document.GLOBALALIAS_PK}`}>
-        <img className="card-img-top" src={props.document.IMAGEURL} alt="NO IMAGE"></img>
+        <img className="card-img-top" src={props.document.IMAGEURL} alt="NO IMAGES"></img>
             <div className="card-body">
                 <h6 className="title-style">{props.document.FULLNAME}</h6>
             </div>
