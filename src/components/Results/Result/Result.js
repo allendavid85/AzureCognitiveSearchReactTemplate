c2a9d97b5940e95058695c7480f19fc60e86546c
allendavid85 <54120819+allendavid85@users.noreply.github.com>
2022-02-22 11:05:02 -0500

    Update Result.js

--- src/components/Results/Result/Result.js
+++ src/components/Results/Result/Result.js
@@ -5,7 +5,7 @@ import './Result.css';
 export default function Result(props) {
     return(
     <div className="card result">
-        <a href={`/details/${props.document.ROWID_OBJECT}`}>
+        <a href={`/details/${props.document.GLOBALALIAS_PK}`}>
             <div className="card-body">
                 <h6 className="title-style">{props.document.FULLNAME}</h6>
             </div>
